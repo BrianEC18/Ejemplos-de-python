08c85b8c7f6d62e5447df27e62df1d6f0eb65c51
BrianEC18 <132420037+BrianEC18@users.noreply.github.com>
2023-10-12 19:06:14 -0600

    [Feature]: Actualizacion de la clase user y la clase student

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,27 +1,28 @@
 class User:
-
-    name=None
-    email=None
-
-    #metodo de enviar correo
+    name = None
+    email = None
+    def _init_(self,name,email):
+        self.name = name
+        self.email = email
     def send_email(self):
         if self.email is not None:
-            print("sending email}:"+self.email)
+            print("sending email: "+self.email)
         else:
-            print("error")
-
+            print("Error")
     def _str_(self):
-        return "User: "+self.name+", "+self.email            
-        
-    #constructor 
-    def _init_(self,name,email):
-        self.name=name
-        self.email=email
-
+        return "User: "+self.name+", "+self.email
+ 
 class Student(User):
-    def _init_(self, name, email, score):
+    score = None
+    def _init_(self, name=None, email=None,score=None):
         super()._init_(name, email)
-        self.score = score
-
+        self.score=score
     def _str_(self):
-        return "Student: "+self.name+", "+self.email+", Score: "+str(self.score)  
+        return "Student: "+self.name+", "+self.email+", Score: "+str(self.score)
+    def _repr_(self):
+        name = self.name
+        email = self.email
+        score = self.score
+        return f"Student({name=},{email=},{score=})"
+    def is_approved(self):
+        return self.score>=8
